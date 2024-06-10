29617dd275f67b810f3a25a541f7de7b74012b67
felip-araujo <felipearaujoexecutive@gmail.com>
2024-06-10 16:10:03 -0400

    09

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+$('#sucesso').hide();
+$('#erro').hide();
+
+
+document.getElementById("formulario").addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const formData = new formData(this); 
+
+    $.ajax({
+        type: "POST",
+        url: "./assets/php/processar_contato.php",
+        data: formData,
+        processData: false, 
+        contentType: false,
+        success: function (response) {
+            $('#sucesso').show(); 
+            $('#sucesso').html(response);
+            $('#erro').hide();
+        },
+        error: function(response){
+            $('#erro').show();
+            $('#erro').html(response);
+            $('#sucesso').hide();
+        } 
+        
+    });    
+})
+
+
--- php/processar_contato.php
+++ php/processar_contato.php
@@ -1,25 +1,40 @@
 <?php 
 
-    if(isset($_POST['enviar'])){
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = isset($_POST['nome']) ? $_POST['nome'] : ''; 
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
 
-        require 'conexao.php';
-        $nome = $_POST['nome'];
-        $email = $_POST['email'];
-        $mensagem_enviada = $_POST['text'];  
+    echo "Dados recebidos"; 
+
+} 
+
+else {
+    http_response_code(400); 
+    echo "mmetodo de requisicao invalido"; 
+}
+
+
+
+    // if(isset($_POST['enviar'])){
+
+    //     require 'conexao.php';
+    //     $nome = $_POST['nome'];
+    //     $email = $_POST['email'];
+    //     $mensagem_enviada = $_POST['text'];  
 
         
          
-        $sql = $pdo->prepare("INSERT INTO formulario_contato (nome, email, mensagem_enviada) VALUES (:nome, :email, :mensagem_enviada)");
-        $sql->bindParam(':nome', $nome);
-        $sql->bindParam(':email', $email);
-        $sql->bindParam(':mensagem_enviada', $mensagem_enviada);
+    //     $sql = $pdo->prepare("INSERT INTO formulario_contato (nome, email, mensagem_enviada) VALUES (:nome, :email, :mensagem_enviada)");
+    //     $sql->bindParam(':nome', $nome);
+    //     $sql->bindParam(':email', $email);
+    //     $sql->bindParam(':mensagem_enviada', $mensagem_enviada);
 
-        if($sql->execute()){
-            echo '<script>alert("Mensagem Enviada")</script>';
-            echo '<script>window.location.href="../index.html#contato"</script>' ;
-        } else {
-            echo " Erro ao enviar sua mensagem";
-        }
-    }
+    //     if($sql->execute()){
+    //         echo '<script>alert("Mensagem Enviada")</script>';
+    //         echo '<script>window.location.href="../index.html#contato"</script>' ;
+    //     } else {
+    //         echo " Erro ao enviar sua mensagem";
+    //     }
+    // }
 
 ?> 
