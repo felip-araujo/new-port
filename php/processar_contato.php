886308cff233ec8f57cb9cc0d520287ef85f110b
felip-araujo <felipearaujoexecutive@gmail.com>
2024-06-17 10:51:59 -0400

    teste

--- php/processar_contato.php
+++ php/processar_contato.php
@@ -1,56 +1,58 @@
 <?php
 
-require '../vendor/autoload.php'; 
+echo 'teste';
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+// require '../vendor/autoload.php'; 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    require 'conexao.php';
-    $dataAtual = date('Y-m-d');
-    $nome = $_POST['nome'] ?? null;
-    $email = $_POST['email'] ?? null;
-    $mensagem = $_POST['text'] ?? null;
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\Exception;
 
-    $stmt = $pdo->prepare("INSERT INTO formulario_contato (nome, email, mensagem_enviada) VALUES (:nome, :email, :mensagem_enviada)");
-    $stmt->bindParam(':nome', $nome);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':mensagem_enviada', $mensagem);
-    if ($stmt->execute()) {
-        echo "Muito obrigado pelo contato, retornarei em breve! " . '<a href="https://wa.me/92984520381"> ou clique aqui para falar no whatsapp</a>';
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//     require 'conexao.php';
+//     $dataAtual = date('Y-m-d');
+//     $nome = $_POST['nome'] ?? null;
+//     $email = $_POST['email'] ?? null;
+//     $mensagem = $_POST['text'] ?? null;
 
-        $mail = new PHPMailer(true);
+//     $stmt = $pdo->prepare("INSERT INTO formulario_contato (nome, email, mensagem_enviada) VALUES (:nome, :email, :mensagem_enviada)");
+//     $stmt->bindParam(':nome', $nome);
+//     $stmt->bindParam(':email', $email);
+//     $stmt->bindParam(':mensagem_enviada', $mensagem);
+//     if ($stmt->execute()) {
+//         echo "Muito obrigado pelo contato, retornarei em breve! " . '<a href="https://wa.me/92984520381"> ou clique aqui para falar no whatsapp</a>';
 
-        try {
-            // Configurações do servidor
-            $mail->isSMTP();
-            $mail->Host       = 'smtp.titan.email'; // Substitua pelo seu host SMTP
-            $mail->SMTPAuth   = true;
-            $mail->Username   = 'felp@evoludesign.com.br'; // Substitua pelo seu email
-            $mail->Password   = 'Fe159753#'; // Substitua pela sua senha
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            $mail->Port       = 587;
+//         $mail = new PHPMailer(true);
 
-            // Destinatários
-            $mail->setFrom('felp@evoludesign.com.br', 'Felipe'); // Substitua pelo seu email e nome
-            $mail->addAddress('felp@evoludesign.com.br'); // Envie para o seu próprio email
+//         try {
+//             // Configurações do servidor
+//             $mail->isSMTP();
+//             $mail->Host       = 'smtp.titan.email'; // Substitua pelo seu host SMTP
+//             $mail->SMTPAuth   = true;
+//             $mail->Username   = 'felp@evoludesign.com.br'; // Substitua pelo seu email
+//             $mail->Password   = 'Fe159753#'; // Substitua pela sua senha
+//             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+//             $mail->Port       = 587;
 
-            // Conteúdo do email
-            $mail->isHTML(true);
-            $mail->Subject = 'Dados do Formulario' . ' | ' . $dataAtual;
-            $mail->Body    = "Nome: $nome<br>Email: $email<br>Mensagem: $mensagem";
-            $mail->AltBody = "Nome: $nome\nEmail: $email\nMensagem: $mensagem";
+//             // Destinatários
+//             $mail->setFrom('felp@evoludesign.com.br', 'Felipe'); // Substitua pelo seu email e nome
+//             $mail->addAddress('felp@evoludesign.com.br'); // Envie para o seu próprio email
 
-            // Envia o email
-            $mail->send();
+//             // Conteúdo do email
+//             $mail->isHTML(true);
+//             $mail->Subject = 'Dados do Formulario' . ' | ' . $dataAtual;
+//             $mail->Body    = "Nome: $nome<br>Email: $email<br>Mensagem: $mensagem";
+//             $mail->AltBody = "Nome: $nome\nEmail: $email\nMensagem: $mensagem";
+
+//             // Envia o email
+//             $mail->send();
             
-        } catch (Exception $e) {
-            echo "Erro ao enviar email: {$mail->ErrorInfo}";
-        }
+//         } catch (Exception $e) {
+//             echo "Erro ao enviar email: {$mail->ErrorInfo}";
+//         }
 
-    } else {
-        echo "Erro ao enviar formulário";
-    }
-} else {
-    echo   'Método de requisiçãoo inválido!';
-}
+//     } else {
+//         echo "Erro ao enviar formulário";
+//     }
+// } else {
+//     echo   'Método de requisiçãoo inválido!';
+// }
